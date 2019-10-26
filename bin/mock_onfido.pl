b32b1b403a749933702767c49864151e077704f9
chylli-binary <chylli@binary.com>
2019-10-26 17:31:22 +0800

    refactor

--- bin/mock_onfido.pl
+++ bin/mock_onfido.pl
@@ -31,13 +31,15 @@ my %checks;
 post '/v2/applicants' => sub {
     my $c         = shift;
     my $data      = $c->req->json;
-    my $applicant = clone($applicant_template);
-    my $id        = $applicant->{id} = Data::UUID->new->create_str();
-    $applicant->{href} = "v2/applicants/$id";
+    my $id        = Data::UUID->new->create_str();
+    my $applicant = {
+        id         => $id,
+        created_at => Date::Utility->new()->datetime_iso8601,
+        href       => "v2/applicants/$id",
+    };
     for my $k (keys %$data) {
         $applicant->{$k} = $data->{$k};
     }
-    $applicant->{created_at} = Date::Utility->new()->datetime_iso8601;
     $applicants{$id} = $applicant;
     $c->render(json => $applicant);
 };
@@ -305,76 +307,6 @@ sub clone_and_remove_private {
     }
     return $result;
 }
-
-$applicant_template = {
-    "id"                  => "123456",
-    "created_at"          => "2014-05-23T13:50:33Z",
-    "delete_at"           => "2018-10-31T04:39:52Z",
-    "href"                => "/v2/applicants/12345",
-    "title"               => "Mr",
-    "first_name"          => "John",
-    "middle_name"         => undef,
-    "last_name"           => "Smith",
-    "email"               => 'john@example.com',
-    "gender"              => "Male",
-    "dob"                 => "1980-01-22",
-    "telephone"           => "02088909293",
-    "mobile"              => undef,
-    "country"             => "GBR",
-    "mothers_maiden_name" => "Johnson",
-    "previous_last_name"  => undef,
-    "nationality"         => "USA",
-    "country_of_birth"    => "USA",
-    "town_of_birth"       => "New York City",
-    "id_numbers"          => [{
-            "type"  => "ssn",
-            "value" => "433-54-3937"
-        },
-        {
-            "type"       => "driving_licence",
-            "value"      => "I1234562",
-            "state_code" => "CA"
-        }
-    ],
-    "addresses" => [{
-            "flat_number"     => undef,
-            "building_name"   => undef,
-            "building_number" => "100",
-            "street"          => "Main Street",
-            "sub_street"      => undef,
-            "state"           => undef,
-            "town"            => "London",
-            "postcode"        => "SW4 6EH",
-            "country"         => "GBR",
-            "start_date"      => "2013-01-01",
-            "end_date"        => undef
-        },
-        {
-            "flat_number"     => "Apt 2A",
-            "building_name"   => undef,
-            "building_number" => "1017",
-            "street"          => "Oakland Ave",
-            "sub_street"      => undef,
-            "town"            => "Piedmont",
-            "state"           => "CA",
-            "postcode"        => "94611",
-            "country"         => "USA",
-            "start_date"      => "2006-03-07",
-            "end_date"        => "2012-12-31"
-        }]};
-
-$check_template = {
-    "id"           => "<CHECK_ID>",
-    "created_at"   => "2014-05-23T13:50:33Z",
-    "href"         => "/v2/applicants/<APPLICANT_ID>/checks/<CHECK_ID>",
-    "type"         => "express",
-    "status"       => "complete",
-    "result"       => "clear",
-    "redirect_uri" => "https://somewhere.else",
-    "results_uri"  => "https://onfido.com/dashboard/information_requests/<REQUEST_ID>",
-    "download_uri" => "https://onfido.com/dashboard/pdf/information_requests/<REQUEST_ID>",
-    "reports"      => ["<REPORT_ID>", "<REPORT_ID>"],
-    "tags"         => ["My tag", "Another tag"]};
 
 # Start the Mojolicious command system
 app->start;
