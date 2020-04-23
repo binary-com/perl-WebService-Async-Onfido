d32bd25ccc89d9ca263e44d47211ad10c5c30f82
Arshad <arshad@binary.com>
2020-04-23 08:00:23 +0000

    just log the response body

--- lib/WebService/Async/Onfido.pm
+++ lib/WebService/Async/Onfido.pm
@@ -695,7 +695,7 @@ sub live_photo_upload {
             $log->errorf('Request %s received %s with full response as %s',
                 $request->uri,
                 $message,
-                $response->as_string(" "),
+                $response->content,
             );
             # Just pass it on
             Future->fail($message, http => $response, $request);
